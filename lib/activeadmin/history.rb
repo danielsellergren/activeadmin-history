f9cf27fd005d74e42e13949ddfa61eb45516523e
Daniel Sellergren <danielsellergren@gmail.com>
2020-05-24 21:59:36 -0400

    Working version including objects and parameters saving

--- lib/activeadmin/history.rb
+++ lib/activeadmin/history.rb
@@ -8,11 +8,17 @@ module ActiveAdmin
 
     def self.included(dsl)
       dsl.controller do
-        before_action :create_admin_action, only: [:create, :update, :destroy]
+        after_action :create_admin_action, only: [:create, :update, :destroy]
 
         def create_admin_action
+          if action_name == 'create'
+          end
+
           ::AdminAction.create(
             admin_user_id: current_admin_user.id,
+            object_id: resource.id,
+            object_type: resource.class.to_s,
+            object_params: params.dig(resource.class.to_s.underscore.to_sym),
             action: params[:action],
           )
         end
