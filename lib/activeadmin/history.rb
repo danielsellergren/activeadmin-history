adda16fe7b4d4fa66f5d6716f288e3851adedcee
Daniel Sellergren <danielsellergren@gmail.com>
2020-05-27 21:57:57 -0400

    Adds diff

--- lib/activeadmin/history.rb
+++ lib/activeadmin/history.rb
@@ -8,19 +8,51 @@ module ActiveAdmin
 
     def self.included(dsl)
       dsl.controller do
-        after_action :create_admin_action, only: [:create, :update, :destroy]
+        before_action :before_admin_action, only: [:create, :update, :destroy]
+        after_action :after_admin_action, only: [:create, :update, :destroy]
 
-        def create_admin_action
-          if action_name == 'create'
+        def before_admin_action
+          @admin_user_id = current_admin_user.id
+          @action = params[:action]
+          @param_attributes = params.dig(controller_name.singularize.to_sym)
+          @object_diff = nil
+
+          if @action == 'update'
+            @object_diff = diff(resource, resource.class.new(@param_attributes.to_unsafe_hash))
           end
 
-          ::AdminAction.create(
-            admin_user_id: current_admin_user.id,
+          @admin_action = ::AdminAction.create(
+            admin_user_id: @admin_user_id,
+            action: @action,
+            object_params: @param_attributes,
+            object_diff: @object_diff,
+          )
+        end
+
+        def after_admin_action
+          @admin_action.update(
             object_id: resource.id,
             object_type: resource.class.to_s,
-            object_params: params.dig(resource.class.to_s.underscore.to_sym),
-            action: params[:action],
           )
+        end
+
+        private
+
+        def diff(resource_a, resource_b)
+          h = {}
+          resource_a.attributes.each_pair do |key, value|
+            if resource_a.respond_to?(key) && resource_b.respond_to?(key)
+              unless %w[id created_at updated_at].include?(key)
+                value_a = resource_a.send(key)
+                value_b = resource_b.send(key)
+                
+                if value_a != value_b
+                  h[key] = [value_a, value_b]
+                end
+              end
+            end  
+          end    
+          h
         end
       end
     end
